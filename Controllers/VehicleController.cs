13a0bd5aa275aa44328ab78d2a77d440998d2129
hajainaHayramaTechnology <160156114+hajainaHayramaTechnology@users.noreply.github.com>
2024-12-18 08:39:41 +0100

    Supprimer les fichiers VS bloqués

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestion_voiture_BackOffice.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index(string name, int count)
+        {
+            ViewData["Name"] = name;
+            ViewData["count"] = count;
+            return View();
+        }
+    }
+}
